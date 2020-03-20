aee186618a639158b071c9b293e6fd6528601ff4
Neylton Benjamim <neylton.b@hotmail.com>
2020-03-20 20:03:03 -0300

    Iniciando o projeto. Foram criado style.css e index.php, o menu já está pronto, menu vai ser fixo no top, e cada link vai abrir um modal. ;

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get User Média</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <div>
+        <div class="menu">
+            <ul>
+                <li><a href="javascript:void(0);">Tirar Foto</a></li>
+                <li><a href="javascript:void(0);">Gravar Vídeo</a></li>
+                <li><a href="javascript:void(0);">Gravar Áudio</a></li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="content">
+                Neylton
+            </div>
+        </div>
+        
+        
+        
+    </div>
+    <div class="full-modal full-modal-hide">
+
+        <div class="modal-tirar-foto"></div>
+        
+        <div class="modal-gravar-video"></div>
+        
+        <div class="modal-gravar-audio"></div>
+    </div>
+</body>
+</html>
