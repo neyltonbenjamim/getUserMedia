57dae94dd3ee17aff206275d6b68b876fdf1f648
Neylton Benjamim <neylton.b@hotmail.com>
2020-04-14 19:37:43 -0300

    Criando estrutura do modal

--- index.php
+++ index.php
@@ -6,32 +6,40 @@
     <title>Get User Média</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/main.js"></script>
+    <script src="js/Media.js"></script>
 </head>
 <body>
     <div>
+
         <div class="menu">
             <ul>
-                <li><a href="javascript:void(0);">Tirar Foto</a></li>
-                <li><a href="javascript:void(0);">Gravar Vídeo</a></li>
-                <li><a href="javascript:void(0);">Gravar Áudio</a></li>
+                <li><a class="open-modal" data-action="foto" href="javascript:void(0);">Tirar Foto</a></li>
+                <li><a class="open-modal" data-action="video" href="javascript:void(0);">Gravar Vídeo</a></li>
+                <li><a class="open-modal" data-action="audio" href="javascript:void(0);">Gravar Áudio</a></li>
             </ul>
         </div>
+
         <div class="container">
             <div class="content">
                 Neylton
             </div>
         </div>
-        
-        
-        
+  
     </div>
     <div class="full-modal full-modal-hide">
 
-        <div class="modal-tirar-foto"></div>
+        <div class="modal modal-hide modal-tirar-foto">
+            <video id="foto"></video>
+        </div>
         
-        <div class="modal-gravar-video"></div>
+        <!-- <div class="modal modal-hide modal-gravar-video">
+            <video id="video"></video>
+        </div>
         
-        <div class="modal-gravar-audio"></div>
+        <div class="modal modal-hide modal-gravar-audio">
+            <audio id="audio"></audio>
+        </div> -->
+
     </div>
 </body>
 </html>
